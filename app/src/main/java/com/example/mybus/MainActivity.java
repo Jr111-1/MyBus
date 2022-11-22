161959122180f437a6dea3500310886a0a030e1d
Jr111-1 <759469355@qq.com>
2022-11-22 18:22:37 +0800

    默认更改列表

--- app/src/main/java/com/example/mybus/MainActivity.java
+++ app/src/main/java/com/example/mybus/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.mybus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button login;
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.login);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password) ;
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String emails = email.getText().toString();
+                String passWord = password.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
