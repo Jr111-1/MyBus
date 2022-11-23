065a95898b7074aa3c0dd9f822c7700817bed743
Jr111-1 <759469355@qq.com>
2022-11-23 13:33:20 +0800

    默认更改列表

---
+++ app/src/main/java/com/example/mybus/MainActivity2.java
@@ -0,0 +1,27 @@
+package com.example.mybus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private Button backBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        backBtn = (Button) findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+}
